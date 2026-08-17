40380ab048159dc59a20f8213085b8ca86d512e1
Rafael Bradley <rafabradleyrb@gmail.com>
2026-08-17 08:28:03 +0000

    Add EnvNyeInstallationEnv

--- src/utils/env.go
+++ src/utils/env.go
@@ -5,4 +5,5 @@ import "os"
 var (
 	EnvNyeInstallationEtc = os.Getenv("NYE_INSTALLATION_ETC")
 	EnvNyeInstallationBin = os.Getenv("NYE_INSTALLATION_BIN")
+	EnvNyeInstallationEnv = os.Getenv("NYE_INSTALLATION_ENV")
 )
