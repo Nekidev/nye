8cd1e9ad09586c90707ed0cf18638b0c3d0f8519
Rafael Bradley <rafabradleyrb@gmail.com>
2026-09-21 07:00:26 +0000

    Add compile error TODOs

--- crates/nye-packages/src/lib.rs
+++ crates/nye-packages/src/lib.rs
@@ -3,6 +3,10 @@
 //! This crate is internally used by `nye`. It is, however, meant to be used as a standalone library
 //! other people can use, and is structured as such.
 
+compile_error!("TODO: Implement writers.");
+compile_error!("TODO: Validate manifests against themselves and against package file directories.");
+compile_error!("TODO: Implement Package::inspect() to provide a nicer API than handling files by hand.");
+
 use crate::manifest::Manifest;
 
 pub mod format;
